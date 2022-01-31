a7a18726dd12aaee3c0d86e9b19350095534da0b
Asif Ali <yoasif0708@gmail.com>
2022-01-31 12:21:09 +0530

    My Lab-Exercises

---
+++ Asif LabExercise/src/LabExercise/AddingNumbers.java
@@ -0,0 +1,15 @@
+package LabExercise;
+
+public class AddingNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		 int first = 700; 
+		int second = 200; 
+		 System.out.println(first + " " + second);
+		int sum = first + second; 
+		 System.out.println("The sum is: " + sum);
+
+	}
+
+}
---
+++ Asif LabExercise/src/LabExercise/HelloWorld.java
@@ -0,0 +1,11 @@
+package LabExercise;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
